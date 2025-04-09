67549b19d36864531829427e1eb0d68a9904dc8e
hritambh <hritambh.hritvij@getvymo.com>
2025-04-10 02:58:31 +0530

    adding DTOs for user login

---
+++ src/main/java/com/xalts/expensetrackerapi/dto/LoginRequest.java
@@ -0,0 +1,9 @@
+package com.xalts.expensetrackerapi.dto;
+
+import lombok.Data;
+
+@Data
+public class LoginRequest {
+    private String email;
+    private String password;
+}
---
+++ src/main/java/com/xalts/expensetrackerapi/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.xalts.expensetrackerapi.repository;
+
+import com.xalts.expensetrackerapi.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
